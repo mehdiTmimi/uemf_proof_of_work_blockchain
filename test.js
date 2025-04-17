3f6930db8b88a11b6e4c4536400d82e4c1eb22cf
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2025-04-17 11:26:12 +0100

    ok

--- test.js
+++ test.js
@@ -1,11 +1,24 @@
 
 
 const Blockchain = require("./models/blockchain");
-const { saveBlockchain } = require("./persistence/blockchainPersitence");
+const { saveBlockchain, loadBlockchain } = require("./persistence/blockchainPersitence");
 
-const blockchain = new Blockchain("Uemf",6,300,50,"uemfCoin")
-saveBlockchain(blockchain)
-.then(()=>console.log("blockchain saved with success"))
-.catch(e=>
-    console.log("error on saving blockchain")
-)
+const testSave = () => {
+    const blockchain = new Blockchain("Uemf", 6, 300, 50, "uemfCoin")
+    saveBlockchain(blockchain)
+        .then(() => console.log("blockchain saved with success"))
+        .catch(e =>
+            console.log("error on saving blockchain")
+        )
+}
+const testLoad = async ()=>{
+   try{
+    const blockchain = await loadBlockchain()
+    console.log(blockchain)
+   }
+   catch(e)
+   {
+    console.log(e)
+   }
+}
+testLoad()
