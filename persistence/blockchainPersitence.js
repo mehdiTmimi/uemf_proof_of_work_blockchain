f6ed768c7c3004c6d4becb463ac2146ef8ee88c9
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2025-04-17 09:47:59 +0100

    ok

--- persistence/blockchainPersitence.js
+++ persistence/blockchainPersitence.js
@@ -1,15 +1,22 @@
 const fs = require("fs")
 const pathDb = "../database/blockchain.json"
-const saveBlockchain = (blockchain)=>{
-    const {name, difficulty, miningInterval, 
-        blockReward, denom, head} = blockchain
+const saveBlockchain = async (blockchain) => {
+    const { name, difficulty, miningInterval,
+        blockReward, denom, head } = blockchain
 
-        if(head!=null){
-            head = head.hash
-        }
-        fs.promises.writeFile(pathDb,JSON.stringify(
+    if (head != null) {
+        head = head.hash
+    }
+    try {
+        await fs.promises.writeFile(pathDb, JSON.stringify(
             {
                 name, difficulty, miningInterval, blockReward, denom, head
             }
         ))
+        return true;
+    }
+    catch (e) {
+        console.error(e)
+        return false
+    }
 }
